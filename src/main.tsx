f289f306f4fd8805f5427c9372e032941ac36812
Tsadkan Kelemework <tsadkankelemeworktw@gmail.com>
2026-02-24 11:12:08 +0300

    ..

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM  from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+const queryClient =new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
