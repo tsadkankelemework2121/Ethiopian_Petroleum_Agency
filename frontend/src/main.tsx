c3968b96d50154c1bb61e8dd179de5c4e064ef63
Tsadkan Kelemework <tsadkankelemeworktw@gmail.com>
2026-04-09 15:08:23 +0300

    deploy

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
-import ReactDOM  from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { AuthProvider } from './context/AuthContext'
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -19,11 +20,11 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+      <BrowserRouter basename="/epa"> 
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
 )
